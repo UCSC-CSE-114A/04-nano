1b1c52721114edb629771ae120a0c8eb3c650936
Shun Kashiwa <shunthedev@gmail.com>
2024-05-13 11:51:28 -0700

    add tests

--- tests/Common.hs
+++ tests/Common.hs
@@ -53,3 +53,6 @@ updateCurrent sc n = modifyIORef sc (\(x, y) -> (x + n, y))
 
 initScore :: IO Score
 initScore = newIORef (0, 0)
+
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (a, b, c) = f a b c
--- tests/Test.hs
+++ tests/Test.hs
@@ -17,80 +17,221 @@ unit sc = testGroup "NANO"
   [ scoreTest ( uncurry Nano.lookupId
               , ("z1", Nano.env0)
               , (Nano.VInt 0)
-              , 5
-              , "lookup1")
+              , 1
+              , "1a - lookup1")
   , scoreTest ( uncurry Nano.lookupId
               ,  ("x", Nano.env0)
               , (Nano.VInt 1)
-              , 5
-              , "lookup2")
+              , 1
+              , "1a - lookup2")
+  , failTest  ( uncurry Nano.lookupId
+              , ("mickey", Nano.env0)
+              , "bound"
+              , 3
+              , "1a - lookup3")
+  , scoreTest ( uncurry3 Nano.extendEnv
+              , ("x", Nano.VInt 5, Nano.env0)
+              , ("x", Nano.VInt 5): Nano.env0
+              , 1
+              , "1a - extend"
+              )
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Plus, Nano.VInt 1, Nano.VInt 2)
+              , Nano.VInt 3
+              , 1
+              , "1b - evalOp Plus"
+              )
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Minus, Nano.VInt 3, Nano.VInt 2)
+              , Nano.VInt 1
+              , 1
+              , "1b - evalOp Minus"
+              )
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Mul, Nano.VInt 2, Nano.VInt 3)
+              , Nano.VInt 6
+              , 1
+              , "1c - evalOp Mul"
+              )
   , scoreTest ( uncurry Nano.eval
               , (Nano.env0, (Nano.EBin Nano.Minus (Nano.EBin Nano.Plus "x" "y") (Nano.EBin Nano.Plus "z" "z1")))
               , (Nano.VInt 0)
-              , 5
-              , "eval EBin 1")
+              , 1
+              , "1d - eval EBin 1")
+  -- part 2
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Eq, Nano.VInt 1, Nano.VInt 1)
+              , Nano.VBool True
+              , 1
+              , "2a - evalOp Eq (VInt 1) (VInt 1)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Eq, Nano.VInt 1, Nano.VInt 2)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp Eq (VInt 1) (VInt 2)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Ne, Nano.VInt 1, Nano.VInt 1)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp Ne (VInt 1) (VInt 1)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Ne, Nano.VInt 1, Nano.VInt 2)
+              , Nano.VBool True
+              , 1
+              , "2a - evalOp Ne (VInt 1) (VInt 2)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Eq, Nano.VBool True, Nano.VBool True)
+              , Nano.VBool True
+              , 1
+              , "2a - evalOp Eq (VBool True) (VBool True)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Eq, Nano.VBool True, Nano.VInt 1)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp Eq (VBool True) (VInt 1)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Lt, Nano.VInt 1, Nano.VInt 2)
+              , Nano.VBool True
+              , 1
+              , "2a - evalOp Lt (VInt 1) (VInt 2)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Lt, Nano.VInt 1, Nano.VInt 1)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp Lt (VInt 1) (VInt 1)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Le, Nano.VInt 2, Nano.VInt 1)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp Le (VInt 2) (VInt 1)")
+  , failTest  ( uncurry3 Nano.evalOp
+              , (Nano.Le, Nano.VBool True, Nano.VBool False)
+              , "type error"
+              , 1
+              , "2a - evalOp Le (VBool True) (VBool False)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.And, Nano.VBool True, Nano.VBool False)
+              , Nano.VBool False
+              , 1
+              , "2a - evalOp And (VBool True) (VBool False)")
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Or, Nano.VBool True, Nano.VBool False)
+              , Nano.VBool True
+              , 1
+              , "2a - evalOp Or (VBool True) (VBool False)")
+  , failTest  ( uncurry3 Nano.evalOp
+              , (Nano.And, Nano.VBool True, Nano.VInt 1)
+              , "type error"
+              , 1
+              , "2a - evalOp And (VBool True) (VInt 1)")
+  , failTest  ( uncurry3 Nano.evalOp
+              , (Nano.Plus, Nano.VBool True, Nano.VInt 1)
+              , "type error"
+              , 1
+              , "2a - evalOp Plus (VBool True) (VInt 1)")
+  -- 2b
+  , scoreTest ( uncurry Nano.eval
+              , ([], Nano.EBool True)
+              , Nano.VBool True
+              , 1
+              , "2b - eval EBool True")
+  , scoreTest ( uncurry Nano.eval
+              , ([], Nano.EBool False)
+              , Nano.VBool False
+              , 1
+              , "2b - eval EBool False")
+  , scoreTest ( uncurry Nano.eval
+              , ([], Nano.EIf (Nano.EBool True) (Nano.EInt 2) (Nano.EBool True))
+              , Nano.VInt 2
+              , 3
+              , "2b - eval EIf True 2 True")
+  , scoreTest ( uncurry Nano.eval
+              , ([], Nano.EIf (Nano.EBool False) (Nano.EInt 2) (Nano.EBool True))
+              , Nano.VBool True
+              , 3
+              , "2b - eval EIf False 2 True")
+  , failTest ( uncurry Nano.eval
+              , ([], Nano.EIf (Nano.EInt 0) (Nano.EInt 2) (Nano.EBool True))
+              , "type error"
+              , 3
+              , "2b - eval EIf 0 2 True")
   , scoreTest ( uncurry Nano.eval
               , ([],(Nano.EBin Nano.Eq (Nano.EInt 2) (Nano.EInt 3)))
               , (Nano.VBool False)
               , 5
-              , "eval EBin 2")
-  , scoreTest ( Nano.eval env1
-              , Nano.EVar "c1"
-              , Nano.VInt 1
-              , 5
-              , "1a - c1")
-  , scoreTest ( Nano.eval env1
-              , Nano.EBin Nano.Mul
-                  ( Nano.EBin Nano.Minus (Nano.EInt 20) (Nano.EVar "c1"))
-                  ( Nano.EBin Nano.Plus  (Nano.EVar "c2") (Nano.EVar "c3") )
-              , Nano.VInt 95
-              , 5
-              , "1a - (20-c1)*(c2+c3)")
-  , failTest  ( Nano.eval env2
-              , Nano.EBin Nano.Plus (Nano.EVar "bt") (Nano.EVar "c3")
-              , "type error"
-              , 6
-              , "1b - True + 3")
-  , failTest  ( Nano.eval env2
-              , Nano.EBin Nano.Or (Nano.EVar "bt") (Nano.EVar "c3")
-              , "type error"
-              , 6
-              , "1b - bt||c3")
-  , scoreTest ( Nano.eval []
-              , Nano.ELet "x" (Nano.EInt 4) (Nano.EVar "x")
-              , Nano.VInt 4
-              , 6
-              , "1c - let x = 4 in x")
-  , scoreTest ( Nano.eval []
-              , Nano.EApp (Nano.ELam "x" (Nano.EBin Nano.Mul (Nano.EVar "x") (Nano.EVar "x"))) (Nano.EInt 5)
-              , Nano.VInt 25
-              , 6
-              , "1d - (fun x->x*x) 5")
+              , "2b - eval EBin 2")
+  , scoreTest ( uncurry Nano.eval
+              , ([], (Nano.ELet "x" (Nano.EInt 1) (Nano.ELet "y" (Nano.EInt 2) (Nano.EBin Nano.Plus (Nano.EVar "x") (Nano.EVar "y")))))
+              , Nano.VInt 3
+              , 3
+              , "3a - eval ELet")
+  , scoreTest ( uncurry Nano.eval
+              , ([], (Nano.EApp (Nano.ELam "x" (Nano.EBin Nano.Plus (Nano.EVar "x") (Nano.EVar "x"))) (Nano.EInt 3)))
+              , Nano.VInt 6
+              , 3
+              , "3b - eval EApp (ELam \"x\" (EBin Plus (EVar \"x\") (EVar \"x\"))) (EInt 3)")
   , scoreTest ( Nano.eval []
               , Nano.ELet "ksum" (Nano.ELam "x" (Nano.EIf (Nano.EBin Nano.Eq (Nano.EVar "x") (Nano.EInt 0)) (Nano.EInt 0) (Nano.EBin Nano.Plus (Nano.EVar "x") (Nano.EApp (Nano.EVar "ksum") (Nano.EBin Nano.Minus (Nano.EVar "x") (Nano.EInt 1)))))) (Nano.EApp (Nano.EVar "ksum") (Nano.EInt 100))
               , Nano.VInt 5050
               , 6
-              , "1e - eval recursion - ksum") -- sum of first k positive integers
+              , "3c - eval recursion - ksum") -- sum of first k positive integers
+  -- Part 4
+  -- `Cons`
+  , scoreTest ( uncurry3 Nano.evalOp
+              , (Nano.Cons, Nano.VInt 1, Nano.VInt 2)
+              , Nano.VPair (Nano.VInt 1) (Nano.VInt 2)
+              , 3
+              , "4a - evalOp Cons (VInt 1) (VInt 2)")
+  -- ENil
+  , scoreTest ( uncurry Nano.eval
+              , ([], Nano.ENil)
+              , Nano.VNil
+              , 3
+              , "4a - eval ENil")
+  -- `head`
+  , scoreTest ( Nano.eval Nano.prelude
+              , Nano.EApp (Nano.EVar "head") (Nano.EBin Nano.Cons (Nano.EInt 1) (Nano.EInt 2))
+              , Nano.VInt 1
+              , 3
+              , "4b - eval EApp head (Cons 1 2)")
+  , failTest  ( Nano.eval Nano.prelude
+              , Nano.EApp (Nano.EVar "head") Nano.ENil
+              , ""
+              , 3
+              , "4b - eval EApp head Nil")
+  -- `tail
+  , scoreTest ( Nano.eval Nano.prelude
+              , Nano.EApp (Nano.EVar "tail") (Nano.EBin Nano.Cons (Nano.EInt 1) (Nano.EInt 2))
+              , Nano.VInt 2
+              , 3
+              , "4b - eval EApp tail (Cons 1 2)")
+  , failTest  ( Nano.eval Nano.prelude
+              , Nano.EApp (Nano.EVar "tail") Nano.ENil
+              , ""
+              , 3
+              , "4b - eval EApp tail Nil")
+  -- mix
   , scoreTest ( Nano.eval Nano.prelude
               , Nano.ELet "len" (Nano.ELam "x" (Nano.EIf (Nano.EBin Nano.Eq (Nano.EVar "x") Nano.ENil) (Nano.EInt 0) (Nano.EBin Nano.Plus (Nano.EInt 1) (Nano.EApp (Nano.EVar "len") (Nano.EApp (Nano.EVar "tail") (Nano.EVar "x")))))) (Nano.EApp (Nano.EVar "len") (Nano.EBin Nano.Cons (Nano.EInt 5) (Nano.EBin Nano.Cons (Nano.EInt 5) (Nano.EBin Nano.Cons (Nano.EInt 5) (Nano.EBin Nano.Cons (Nano.EInt 5) Nano.ENil)))))
               , Nano.VInt 4
               , 6
-              , "1f - recursion + VPrim, list length") -- length of a list
+              , "4b - recursion + VPrim, list length") -- length of a list
   , scoreTest ( uncurry Nano.eval
               , (Nano.prelude, (parse "let x = [1,2,3,4] in head (tail x)"))
               , Nano.VInt 2
               , 6
-              , "1f - VPrim 1")
+              , "4b - head of tail")
   , scoreTest ( uncurry Nano.eval
               , (Nano.prelude, (parse "let x = [1,2,3,4] in head [2,3]:tail x"))
               , Nano.valueList [Nano.VInt 2, Nano.VInt 2, Nano.VInt 3, Nano.VInt 4]
               , 6
-              , "1f - VPrim 2")
+              , "4b - head and tail")
   , scoreTest ( uncurry Nano.eval
               , (Nano.prelude, (parse "let sumList x s = if x == [] then s else sumList (tail x) (s + head x) in sumList [1,2,3,4] 0"))
               , Nano.VInt 10
               , 6
-              , "1e/f - sumList tail recursion") -- sum of a list
+              , "4b - tail-recursive sum list") -- sum of a list
+  -- integration tests
   , fileTest  ( "tests/input/t1.hs"
               , Nano.VInt 45
               , 6 )
